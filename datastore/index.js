6535cfe2649acf7692754be42cfa043821c43ae0
gchen1196 <gchen1196@gmail.com>
2019-04-30 15:25:34 -0700

    Tests passing for readOne, Update, and Delete.

--- datastore/index.js
+++ datastore/index.js
@@ -10,7 +10,6 @@ var items = {};
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, id) =>{
   
-    // console.log('THIS IS ID' , id);
     fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
       if (err) { 
         throw err; 
@@ -21,42 +20,56 @@ exports.create = (text, callback) => {
   });
 };
       
-
+//iterates through items array 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
-  });
-  callback(null, data);
+  fs.readdir(`${exports.dataDir}`, (err, files) => {
+    if(err) {
+      console.log(`No Files found in ${exports.dataDir}`);
+    } else {
+      callback(null, files);
+    }
+  })
+  // callback(null, data);
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err,data) => {
+    if(err) {
+      callback('file not found');
+    } else {
+      callback(null, {id: id, text: data});
+    }
+  });
 };
 
+//id = '00001'
+//text = 'string'
+//callback
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+        if (err) {
+          callback(err);
+        }else {
+          callback(null, text)
+        }
+      })
+    }
+  })
 };
+      
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+ fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+   if(err) {
+     callback(err);
+   } else {
+     callback()
+   }
+ })
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
